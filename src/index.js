10db22b6cbdbe3b96d2cfd444455895fb7c573ae
spiderMonk <2140113@sliet.ac.in>
2024-06-10 00:47:50 +0530

    finalised the file structure modification

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv"
 import { app } from "./app.js"
-import ConnectDB from "../db/index.js"
+import ConnectDB from "./db/index.js"
 dotenv.config({ path: "./env" })
 
 ConnectDB()
