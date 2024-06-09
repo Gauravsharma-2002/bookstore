588291f0bc09d8aa6b0499a5e6c0c24407464cee
spiderMonk <gauravmaharaj1234@gmail.com>
2024-06-09 18:14:32 +0530

    created connection instance of db and called it on app launch

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
-import dotenv from "dotenv";
-import { app } from "./app.js";
+import dotenv from "dotenv"
+import { app } from "./app.js"
+import ConnectDB from "../db/index.js"
+dotenv.config({ path: "./env" })
 
-dotenv.config({ path: "./env" });
-
+ConnectDB()
+    .then(() => {
+        app.on("error", (err) => {
+            console.log("Error occured while connecting to server")
+            throw err
+        })
+        const server = app.listen(process.env.PORT || 8080, () => {
+            console.log(
+                `server running appropriately on PORT : ${process.env.PORT}`
+            )
+        })
+    })
+    .catch((err) => {
+        console.log("Error occured while connecting to server")
+        throw err
+    })
